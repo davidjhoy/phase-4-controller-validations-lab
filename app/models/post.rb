bb3ee9a7ab38eca71db877b3ec4d5b06e3096045
david <davidjhoyjr@gmail.com>
2022-07-17 21:12:36 -0400

    commit tree 32c8dadfe86f07f279703ed7f9f099c2f17edeed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :content, length: {minimum: 100}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validates :title, presence: true
 end
